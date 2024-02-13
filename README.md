484e61466b976a3a9e656e1a8b41761452f49b1e
Jonathas Dias <jonathas.dev446@gmail.com>
2024-02-12 22:07:06 -0300

    Correcoes e melhorias adicionadas

--- README.md
+++
@@ -1,2 +0,0 @@
-# gerenciador-de-musicas
-Esse é um site de gerenciamento de musicas, desenvolvido com html, tailwindcss e javascript. Acesse: https://gerenciador-de-musicas.netlify.app
---
+++ src/javascript/music.js
@@ -0,0 +1,111 @@
+const BattleScars = {
+    id: 1,
+    songName: 'Battle Scars',
+    song: './music/Battle Scars.mp3',
+    songImg: './imgs/mountains.webp',
+    singerName: 'Lupe Fiasco'
+}
+
+const Grenade = {
+    id: 2,
+    songName: 'Grenade',
+    song: './music/Grenade.mp3',
+    songImg: './imgs/man.webp',
+    singerName: 'Bruno Mars'
+}
+
+const Imprevisto = {
+    id: 3,
+    songName: 'Imprevisto',
+    song: './music/Imprevisto.mp3',
+    songImg: './imgs/ink.webp',
+    singerName: 'Yago Oproprio'
+}
+
+const Linkin = {
+    id: 4,
+    songName: 'In The End',
+    song: './music/Linkin.mp3',
+    songImg: './imgs/guitar.jpg',
+    singerName: 'Mellen Gi & Tommee Profitt'
+}
+
+const Pablo = {
+    id: 5,
+    songName: 'No frio da solidão',
+    song: './music/Pablo.mp3',
+    songImg: './imgs/owl.webp',
+    singerName: 'Pablo'
+}
+
+const Polozhenie = {
+    id: 6,
+    songName: 'Polozhenie',
+    song: './music/Polozhenie.mp3',
+    songImg: './imgs/fantasy.webp',
+    singerName: 'Ravens Rock'
+}
+
+const EnquantoVoceDormia = {
+    id: 7,
+    songName: 'Enquanto Você Dormia',
+    song: './music/Enquanto Você Dormia.mp3',
+    songImg: './imgs/radio.webp',
+    singerName: 'Projota'
+}
+
+const PraNaoDizerQueNaoFaleiDoOdio = {
+    id: 8,
+    songName: 'Pra Não Dizer Que Não Falei Do Ódio',
+    song: './music/Pra Não Dizer Que Não Falei Do Ódio.mp3',
+    songImg: './imgs/radio.webp',
+    singerName: 'Projota'
+}
+
+const Samurai = {
+    id: 9,
+    songName: 'Samurai',
+    song: './music/Samurai.mp3',
+    songImg: './imgs/radio.webp',
+    singerName: 'Projota'
+}
+
+const Runnin = {
+    id: 10,
+    songName: 'Runnin',
+    song: './music/Runnin.mp3',
+    songImg: './imgs/mobile.jpg',
+    singerName: 'Adam Lambert'
+}
+
+const Dynasty = {
+    id: 11,
+    songName: 'Dynasty',
+    song: './music/Dynasty.mp3',
+    songImg: './imgs/equalizer.webp',
+    singerName: 'MIIA'
+}
+
+const TheScientist = {
+    id: 12,
+    songName: 'The Scientist',
+    song: './music/The Scientist.mp3',
+    songImg: './imgs/mic.jpg',
+    singerName: 'Boyce Avenue & Hannah Trigwell'
+}
+
+const music = [
+    BattleScars,
+    Grenade,
+    Imprevisto,
+    Linkin,
+    Pablo,
+    Polozhenie,
+    EnquantoVoceDormia,
+    PraNaoDizerQueNaoFaleiDoOdio,
+    Samurai,
+    Runnin,
+    Dynasty,
+    TheScientist
+];
+export default music;
